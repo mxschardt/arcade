4b972a9e7fb930554c24c4c23bbce88881ba7632
Maxim Schardt <mxschardt@gmail.com>
2022-09-05 23:06:04 +0300

    fix: Off by one error fixed

--- src/lib.rs
+++ src/lib.rs
@@ -25,8 +25,8 @@ impl Minesweeper {
             mines: {
                 let mut mines = HashSet::new();
                 while mines.len() < mine_count {
-                    let rand_width = rand::thread_rng().gen_range(0..=width);
-                    let rand_height = rand::thread_rng().gen_range(0..=height);
+                    let rand_width = rand::thread_rng().gen_range(0..width);
+                    let rand_height = rand::thread_rng().gen_range(0..height);
 
                     mines.insert((rand_width, rand_height));
                 }
@@ -52,15 +52,15 @@ impl Minesweeper {
 
     fn get_neighbors_pos(&self, (x, y): Position) -> impl Iterator<Item = Position> {
         let x_min = if x > 0 { x - 1 } else { x };
-        let x_max = if x < self.height { x + 1 } else { x };
+        let x_max = if x >= self.width { x } else { x + 2 };
         let y_min = if y > 0 { y - 1 } else { y };
-        let y_max = if y < self.width { y + 1 } else { y };
+        let y_max = if y >= self.height { y } else { y + 2 };
 
         (x_min..x_max)
             .flat_map(move |i| (y_min..y_max).map(move |j| (i, j)))
-            .filter(|&(i, j)| i != j)
+            .filter(move |&pos| pos != (x, y))
     }
-    // Only non-mines positions expected 
+    // Only non-mines positions expected
     fn count_mines(&self, p: Position) -> u8 {
         self.get_neighbors_pos(p).fold(0, |acc, item| {
             if self.mines.contains(&item) {
@@ -84,11 +84,24 @@ mod tests {
 
     #[test]
     fn setup() {
-        let ms = Minesweeper::new(10, 10, 5);
-
+        let mut ms = Minesweeper::new(4, 4, 5);
+        for i in 0..ms.height {
+            for j in 0..ms.width {
+                ms.reveal_cell((i, j));
+            }
+        }
+        println!("{}", ms);
         println!("{:?}", ms);
     }
 
+    #[test]
+    fn test_neighbour_pos() {
+        let ms = Minesweeper::new(2, 2, 0);
+
+        assert_eq!(3, ms.get_neighbors_pos((0, 0)).count());
+        assert_eq!(5, ms.get_neighbors_pos((0, 1)).count());
+        assert_eq!(8, ms.get_neighbors_pos((1, 1)).count());
+    }
     #[test]
     fn test_counting_mines() {
         let empty_ms = Minesweeper::new(10, 10, 0);
